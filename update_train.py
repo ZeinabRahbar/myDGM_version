34d78bdce5160c1d9dad590cda0abedec10c920c
Zeinab Rahbar <90860318+ZeinabRahbar@users.noreply.github.com>
2023-12-01 15:01:50 +0330

    update_train.py

--- update_train.py
+++ update_train.py
@@ -1,40 +1,38 @@
 import os
 import torch
 import numpy as np
-
 import torch_geometric
 from torch import nn
 from torch.nn import Module, ModuleList, Sequential
 from torch_geometric.nn import EdgeConv, DenseGCNConv, DenseGraphConv, GCNConv, GATConv
 from torch.utils.data import DataLoader
-
 import pytorch_lightning as pl
 from argparse import Namespace
-
-import torch
 import numpy
 import torch
 import pykeops
 from pykeops.torch import LazyTensor
 from torch.nn import Module, ModuleList, Sequential
 from torch import nn
-
-#Euclidean distance
+from torch.nn import Module, ModuleList, Sequential
+from torch import nn
+from torch.utils.data import DataLoader
+from argparse import ArgumentParser
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from pytorch_lightning.loggers import TensorBoardLogger
+import os.path as osp
+from torch_geometric.datasets import Planetoid
+import torch_geometric.transforms as T
 def pairwise_euclidean_distances(x, dim=-1):
-    dist = torch.cdist(x,x)**2
-    return dist, x
-
-# #Poincarè disk distance r=1 (Hyperbolic)
+    return torch.cdist(x,x)**2, x
 def pairwise_poincare_distances(x, dim=-1):
     x_norm = (x**2).sum(dim,keepdim=True)
     x_norm = (x_norm.sqrt()-1).relu() + 1 
     x = x/(x_norm*(1+1e-2))
     x_norm = (x**2).sum(dim,keepdim=True)
-    
     pq = torch.cdist(x,x)**2
     dist = torch.arccosh(1e-6+1+2*pq/((1-x_norm)*(1-x_norm.transpose(-1,-2))))**2
     return dist, x
- 
 class MLP(nn.Module): 
     def __init__(self, layers_size,final_activation=False, dropout=0):
         super(MLP, self).__init__()
@@ -47,100 +45,73 @@ class MLP(nn.Module):
                 continue
             layers.append(nn.LeakyReLU(0.1))
         self.MLP = nn.Sequential(*layers)
-        
     def forward(self, x, e=None):
         x = self.MLP(x)
         return x
-    
 class Identity(nn.Module):
     def __init__(self,retparam=None):
         self.retparam=retparam
         super(Identity, self).__init__()
-        
     def forward(self, *params):
         if self.retparam is not None:
             return params[self.retparam]
         return params
-
-from torch.nn import Module, ModuleList, Sequential
-from torch import nn
-
 class DGM_d(nn.Module):
     def __init__(self, embed_f, k=5, distance=pairwise_euclidean_distances, sparse=True):
         super(DGM_d, self).__init__()
-        
         self.sparse=sparse
-        
         self.temperature = nn.Parameter(torch.tensor(1. if distance=="hyperbolic" else 4.).float())
         self.embed_f = embed_f
         self.centroid=None
         self.scale=None
         self.k = k
-        
         self.debug=False
         if distance == 'euclidean':
             self.distance = pairwise_euclidean_distances
         else:
             self.distance = pairwise_poincare_distances
-        
     def forward(self, x, A, not_used=None, fixedges=None):
         x = self.embed_f(x,A)  
-        
         if self.training:
             if fixedges is not None:                
                 return x, fixedges, torch.zeros(fixedges.shape[0],fixedges.shape[-1]//self.k,self.k,dtype=torch.float,device=x.device)
-            
             D, _x = self.distance(x)
-           
             #sampling here
             edges_hat, logprobs = self.sample_without_replacement(D)
-                
         else:
             with torch.no_grad():
                 if fixedges is not None:                
                     return x, fixedges, torch.zeros(fixedges.shape[0],fixedges.shape[-1]//self.k,self.k,dtype=torch.float,device=x.device)
                 D, _x = self.distance(x)
-
                 #sampling here
                 edges_hat, logprobs = self.sample_without_replacement(D)
-
         if self.debug:
             self.D = D
             self.edges_hat=edges_hat
             self.logprobs=logprobs
             self.x=x
-
         return x, edges_hat, logprobs
-    
     def sample_without_replacement(self, logits):
         b,n,_ = logits.shape
         logits = logits * torch.exp(torch.clamp(self.temperature,-5,5))
-        
         q = torch.rand_like(logits) + 1e-8
         lq = (logits-torch.log(-torch.log(q)))
         logprobs, indices = torch.topk(-lq,self.k)  
-    
         rows = torch.arange(n).view(1,n,1).to(logits.device).repeat(b,1,self.k)
         edges = torch.stack((indices.view(b,-1),rows.view(b,-1)),-2)
-        
         if self.sparse:
             return (edges+(torch.arange(b).to(logits.device)*n)[:,None,None]).transpose(0,1).reshape(2,-1), logprobs
         return edges, logprobs
-    
 class DGM_Model(pl.LightningModule):
     def __init__(self, hparams):
         super(DGM_Model,self).__init__()
-        
         if type(hparams) is not Namespace:
             hparams = Namespace(**hparams)
-        
         self.save_hyperparameters(hparams)
         conv_layers = hparams.conv_layers
         fc_layers = hparams.fc_layers
         dgm_layers = hparams.dgm_layers
         k = hparams.k
-
-            
         self.graph_f = ModuleList() 
         self.node_g = ModuleList() 
         for i,(dgm_l,conv_l) in enumerate(zip(dgm_layers,conv_layers)):
@@ -164,19 +135,15 @@ class DGM_Model(pl.LightningModule):
                 self.node_g.append(GCNConv(conv_l[0],conv_l[1]))
             if hparams.gfun == 'gat':
                 self.node_g.append(GATConv(conv_l[0],conv_l[1]))
-        
         self.fc = MLP(fc_layers, final_activation=False)
         if hparams.pre_fc is not None and len(hparams.pre_fc)>0:
             self.pre_fc = MLP(hparams.pre_fc, final_activation=True)
         self.avg_accuracy = None
-        
         self.automatic_optimization = False
         self.debug=False
-        
     def forward(self,x, edges=None):
         if self.hparams.pre_fc is not None and len(self.hparams.pre_fc)>0:
             x = self.pre_fc(x)
-            
         graph_x = x.detach()
         lprobslist = []
         for f,g in zip(self.graph_f, self.node_g):
@@ -187,61 +154,42 @@ class DGM_Model(pl.LightningModule):
             graph_x = torch.cat([graph_x,x.detach()],-1)
             if lprobs is not None:
                 lprobslist.append(lprobs)
-                
         return self.fc(x),torch.stack(lprobslist,-1) if len(lprobslist)>0 else None
-   
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
         return optimizer
-
     def training_step(self, train_batch, batch_idx):
-        
         optimizer = self.optimizers(use_pl_optimizer=True)
         optimizer.zero_grad()
-        
         X, y, mask, edges = train_batch
         edges = edges[0]
-        
         assert(X.shape[0]==1) #only works in transductive setting
         mask=mask[0]
-        
         pred,logprobs = self(X,edges)
-        
         train_pred = pred[:,mask.to(torch.bool),:]
         train_lab = y[:,mask.to(torch.bool),:]
-
         loss = torch.nn.functional.binary_cross_entropy_with_logits(train_pred,train_lab)
         loss.backward()
-
         correct_t = (train_pred.argmax(-1) == train_lab.argmax(-1)).float().mean().item()
-
         if logprobs is not None: 
             corr_pred = (train_pred.argmax(-1)==train_lab.argmax(-1)).float().detach()
             wron_pred = (1-corr_pred)
-
             if self.avg_accuracy is None:
                 self.avg_accuracy = torch.ones_like(corr_pred)*0.5
-
             point_w = (self.avg_accuracy-corr_pred)#*(1*corr_pred + self.k*(1-corr_pred))
             graph_loss = point_w * logprobs[:,mask.to(torch.bool),:].exp().mean([-1,-2])
-
             graph_loss = graph_loss.mean()# + self.graph_f[0].Pr.abs().sum()*1e-3
             graph_loss.backward()
-            
             self.log('train_graph_loss', graph_loss.detach().cpu())
             if(self.debug):
                 self.point_w = point_w.detach().cpu()
-                
             self.avg_accuracy = self.avg_accuracy.to(corr_pred.device)*0.95 +  0.05*corr_pred
-            
         optimizer.step()
         self.log('train_acc', correct_t)
         self.log('train_loss', loss.detach().cpu())
-            
     def test_step(self, train_batch, batch_idx):
         X, y, mask, edges = train_batch
         edges = edges[0]
-        
         assert(X.shape[0]==1) #only works in transductive setting
         mask=mask[0]
         pred,logprobs = self(X,edges)
@@ -255,38 +203,23 @@ class DGM_Model(pl.LightningModule):
         loss = torch.nn.functional.binary_cross_entropy_with_logits(test_pred,test_lab)
         self.log('test_loss', loss.detach().cpu())
         self.log('test_acc', 100*correct_t)
-    
     def validation_step(self, train_batch, batch_idx):
         X, y, mask, edges = train_batch
         edges = edges[0]
-        
-        
         assert(X.shape[0]==1) #only works in transductive setting
         mask=mask[0]
-        
         pred,logprobs = self(X,edges)
         pred = pred.softmax(-1)
         for i in range(1,self.hparams.test_eval):
             pred_,logprobs = self(X,edges)
             pred+=pred_.softmax(-1)
-        
         test_pred = pred[:,mask.to(torch.bool),:]
         test_lab = y[:,mask.to(torch.bool),:]
         correct_t = (test_pred.argmax(-1) == test_lab.argmax(-1)).float().mean().item()
         loss = torch.nn.functional.binary_cross_entropy_with_logits(test_pred,test_lab)
-        
         self.log('val_loss', loss.detach())
         self.log('val_acc', 100*correct_t)
-
 import sys
-import torch
-import pickle
-import numpy as np
-import os.path as osp
-import torch
-from torch_geometric.datasets import Planetoid
-import torch_geometric.transforms as T
- 
 def get_planetoid_dataset(name, normalize_features=True, transform=None, split="complete"):
     path = osp.join('.', 'data', name)
     if split == 'complete':
@@ -306,11 +239,9 @@ def get_planetoid_dataset(name, normalize_features=True, transform=None, split="
     elif transform is not None:
         dataset.transform = transform
     return dataset
-
 def one_hot_embedding(labels, num_classes):
     y = torch.eye(num_classes) 
     return y[labels] 
-
 class PlanetoidDataset(torch.utils.data.Dataset):
     def __init__(self, split='train', samples_per_epoch=100, name='Cora', device='cpu'):
         dataset = get_planetoid_dataset(name)
@@ -319,52 +250,30 @@ class PlanetoidDataset(torch.utils.data.Dataset):
         self.edge_index = dataset[0].edge_index.to(device)
         self.n_features = dataset[0].num_node_features
         self.num_classes = dataset.num_classes
-        
         if split=='train':
             self.mask = dataset[0].train_mask.to(device)
         if split=='val':
             self.mask = dataset[0].val_mask.to(device)
         if split=='test':
             self.mask = dataset[0].test_mask.to(device)
-         
         self.samples_per_epoch = samples_per_epoch
-
     def __len__(self):
         return self.samples_per_epoch
-
     def __getitem__(self, idx):
         return self.X,self.y,self.mask,self.edge_index
-
 os.environ["CUDA_VISIBLE_DEVICES"]="0";
-
-import pickle
-import numpy as np
-
-import torch
-from torch.utils.data import DataLoader
-import pytorch_lightning as pl
-
-from argparse import ArgumentParser
-from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
-from pytorch_lightning.loggers import TensorBoardLogger
-
 def run_training_process(run_params):
-    
     train_data = None
     test_data = None
-    
     if run_params.dataset in ['Cora', 'CiteSeer', 'PubMed']:
         train_data = PlanetoidDataset(split='train', name=run_params.dataset, device='cuda')
         val_data = PlanetoidDataset(split='val', name=run_params.dataset, samples_per_epoch=1)
-        test_data = PlanetoidDataset(split='test', name=run_params.dataset, samples_per_epoch=1)
-                           
+        test_data = PlanetoidDataset(split='test', name=run_params.dataset, samples_per_epoch=1)       
     if train_data is None:
         raise Exception("Dataset %s not supported" % run_params.dataset)
-        
     train_loader = DataLoader(train_data, batch_size=1,num_workers=0)
     val_loader = DataLoader(val_data, batch_size=1)
     test_loader = DataLoader(test_data, batch_size=1)
-
     class MyDataModule(pl.LightningDataModule):
         def setup(self,stage=None):
             pass
@@ -374,8 +283,6 @@ def run_training_process(run_params):
             return val_loader
         def test_dataloader(self):
             return test_loader
-    
-    #configure input feature size
     if run_params.pre_fc is None or len(run_params.pre_fc)==0: 
         if len(run_params.dgm_layers[0])>0:
             run_params.dgm_layers[0][0]=train_data.n_features
@@ -383,9 +290,7 @@ def run_training_process(run_params):
     else:
         run_params.pre_fc[0]=train_data.n_features
     run_params.fc_layers[-1] = train_data.num_classes
-    
     model = DGM_Model(run_params)
-
     checkpoint_callback = ModelCheckpoint(
         save_last=True,
         save_top_k=1,
@@ -400,18 +305,13 @@ def run_training_process(run_params):
         verbose=False,
         mode='min')
     callbacks = [checkpoint_callback,early_stop_callback]
-    
     if val_data==test_data:
         callbacks = None
-        
     logger = TensorBoardLogger("logs/")
-    trainer = pl.Trainer.from_argparse_args(run_params,logger=logger,
-                                            callbacks=callbacks) 
+    trainer = pl.Trainer.from_argparse_args(run_params,logger=logger,callbacks=callbacks) 
     trainer.fit(model, datamodule=MyDataModule())
     trainer.test()
-    
 if __name__ == "__main__":
-
     parser = ArgumentParser()
     parser = pl.Trainer.add_argparse_args(parser)
     params = parser.parse_args(['--gpus','1',                         
@@ -420,25 +320,19 @@ if __name__ == "__main__":
                               '--progress_bar_refresh_rate','10',                         
                               '--check_val_every_n_epoch','1'])
     parser.add_argument("--num_gpus", default=10, type=int)
-    
     parser.add_argument("--dataset", default='Cora')
-    parser.add_argument("--fold", default='0', type=int) #Used for k-fold cross validation in tadpole/ukbb
-    
     parser.add_argument("--conv_layers", default=[[32,32],[32,16],[16,8]], type=lambda x :eval(x))
     parser.add_argument("--dgm_layers", default= [[32,16,4],[],[]], type=lambda x :eval(x))
     parser.add_argument("--fc_layers", default=[8,8,3], type=lambda x :eval(x))
     parser.add_argument("--pre_fc", default=[-1,32], type=lambda x :eval(x))
-
     parser.add_argument("--gfun", default='gcn')
     parser.add_argument("--ffun", default='gcn')
     parser.add_argument("--k", default=5, type=int) 
     parser.add_argument("--pooling", default='add')
     parser.add_argument("--distance", default='euclidean')
-
     parser.add_argument("--dropout", default=0.0, type=float)
     parser.add_argument("--lr", default=1e-2, type=float)
     parser.add_argument("--test_eval", default=10, type=int)
-
     parser.set_defaults(default_root_path='./log')
     params = parser.parse_args(namespace=params)
     run_training_process(params)
