b272b6a6b7caa2a16ae862a1cf632bd54a0e1e78
Zeinab Rahbar <90860318+ZeinabRahbar@users.noreply.github.com>
2024-04-27 17:37:39 +0330

    Changed_code.py

--- Changed_code.py
+++ Changed_code.py
@@ -84,6 +84,117 @@ class DGM_d(nn.Module):
         if self.sparse:
             return (edges+(torch.arange(b).to(logits.device)*n)[:,None,None]).transpose(0,1).reshape(2,-1), logprobs
         return edges, logprobs
+
+
+class DGM_d(nn.Module):
+    def __init__(self, embed_f, k=5, distance="euclidean", sparse=True):
+        super(DGM_d, self).__init__()
+        
+        self.sparse=sparse
+        
+        self.temperature = nn.Parameter(torch.tensor(1. if distance=="hyperbolic" else 4.).float())
+        self.embed_f = embed_f
+        self.centroid=None
+        self.scale=None
+        self.k = k
+        self.distance = distance
+        
+        self.debug=False
+        
+    def forward(self, x, A, not_used=None, fixedges=None):
+        if x.shape[0]==1:
+            x = x[0]
+        x = self.embed_f(x,A)
+        if x.dim()==2:
+            x = x[None,...]
+    
+        if self.training:
+            if fixedges is not None:                
+                return x, fixedges, torch.zeros(fixedges.shape[0],fixedges.shape[-1]//self.k,self.k,dtype=torch.float,device=x.device)
+            #sampling here
+            edges_hat, logprobs = self.sample_without_replacement(x)
+                
+        else:
+            with torch.no_grad():
+                if fixedges is not None:                
+                    return x, fixedges, torch.zeros(fixedges.shape[0],fixedges.shape[-1]//self.k,self.k,dtype=torch.float,device=x.device)
+                #sampling here
+                edges_hat, logprobs = self.sample_without_replacement(x)
+              
+        if self.debug:
+            if self.distance=="euclidean":
+                D, _x = pairwise_euclidean_distances(x)
+            if self.distance=="hyperbolic":
+                D, _x = pairwise_poincare_distances(x)
+                
+            self.D = (D * torch.exp(torch.clamp(self.temperature,-5,5))).detach().cpu()
+            self.edges_hat=edges_hat.detach().cpu()
+            self.logprobs=logprobs.detach().cpu()
+
+        return x, edges_hat, logprobs
+    
+
+    def sample_without_replacement(self, x):
+        
+        b,n,_ = x.shape
+        
+        if self.distance=="euclidean":
+            G_i = LazyTensor(x[:, :, None, :])    # (M**2, 1, 2)
+            X_j = LazyTensor(x[:, None, :, :])    # (1, N, 2)
+        
+            mD = ((G_i - X_j) ** 2).sum(-1)
+
+            #argKmin already add gumbel noise
+            lq = mD * torch.exp(torch.clamp(self.temperature,-5,5))
+            indices = lq.argKmin(self.k, dim=1)
+
+            x1 = torch.gather(x, -2, indices.view(indices.shape[0],-1)[...,None].repeat(1,1,x.shape[-1]))
+            x2 = x[:,:,None,:].repeat(1,1,self.k,1).view(x.shape[0],-1,x.shape[-1])
+            logprobs = (-(x1-x2).pow(2).sum(-1) * torch.exp(torch.clamp(self.temperature,-5,5))).reshape(x.shape[0],-1,self.k)
+
+        if self.distance=="hyperbolic":
+            pass
+            x_norm = (x**2).sum(-1,keepdim=True)
+            x_norm = (x_norm.sqrt()-1).relu() + 1 
+            x = x/(x_norm*(1+1e-2)) #safe distance to the margin
+            x_norm = (x**2).sum(-1,keepdim=True)
+                
+            G_i = LazyTensor(x[:, :, None, :])    # (M**2, 1, 2)
+            X_j = LazyTensor(x[:, None, :, :])    # (1, N, 2)
+
+            G_i2 = LazyTensor(1-x_norm[:, :, None, :])    # (M**2, 1, 2)
+            X_j2 = LazyTensor(1-x_norm[:, None, :, :])    # (1, N, 2)
+
+            pq = ((G_i - X_j) ** 2).sum(-1)
+            N = (G_i2*X_j2)
+            XX = (1e-6+1+2*pq/N)
+            mD = (XX+(XX**2-1).sqrt()).log()**2
+
+            lq = mD * torch.exp(torch.clamp(self.temperature,-5,5))
+            indices = lq.argKmin(self.k, dim=1)
+
+            x1 = torch.gather(x, -2, indices.view(indices.shape[0],-1)[...,None].repeat(1,1,x.shape[-1]))
+            x2 = x[:,:,None,:].repeat(1,1,self.k,1).view(x.shape[0],-1,x.shape[-1])
+
+            x1_n = torch.gather(x_norm, -2, indices.view(indices.shape[0],-1)[...,None].repeat(1,1,x_norm.shape[-1]))
+            x2_n = x_norm[:,:,None,:].repeat(1,1,self.k,1).view(x.shape[0],-1,x_norm.shape[-1])
+
+            pq = (x1-x2).pow(2).sum(-1)
+            pqn = ((1-x1_n)*(1-x2_n)).sum(-1)
+            XX = 1e-6+1+2*pq/pqn
+            dist = torch.log(XX+(XX**2-1).sqrt())**2
+            logprobs = (-dist * torch.exp(torch.clamp(self.temperature,-5,5))).reshape(x.shape[0],-1,self.k)
+
+            if self.debug:
+                self._x=x.detach().cpu()+0
+
+        rows = torch.arange(n).view(1,n,1).to(x.device).repeat(b,1,self.k)
+        edges = torch.stack((indices.view(b,-1),rows.view(b,-1)),-2)
+
+        if self.sparse:
+            return (edges+(torch.arange(b).to(x.device)*n)[:,None,None]).transpose(0,1).reshape(2,-1), logprobs
+        return edges, logprobs
+        
 class DGM_Model(pl.LightningModule):
     def __init__(self, hparams):
         super(DGM_Model,self).__init__()
