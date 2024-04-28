8d129330af63a7ec231a878dc44d5cc5a26537e6
Zeinab Rahbar <90860318+ZeinabRahbar@users.noreply.github.com>
2024-04-28 21:07:40 +0330

    Changed_code.py

--- Changed_code.py
+++ Changed_code.py
@@ -18,8 +18,10 @@ import torch_geometric.transforms as T
 from argparse import ArgumentParser
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from pytorch_lightning.loggers import TensorBoardLogger
+
 def pairwise_euclidean_distances(x, dim=-1):
     return torch.cdist(x,x)**2, x
+
 class MLP(nn.Module):
     def __init__(self, layers_size,final_activation=False, dropout=0):
         super(MLP, self).__init__()
@@ -43,62 +45,18 @@ class Identity(nn.Module):
         if self.retparam is not None:
             return params[self.retparam]
         return params
-class DGM_d(nn.Module):
-    def __init__(self, embed_f, k=5, distance=pairwise_euclidean_distances, sparse=True):
-        super(DGM_d, self).__init__()
-        self.sparse=sparse
-        self.temperature = nn.Parameter(torch.tensor(1. if distance=="hyperbolic" else 4.).float())
-        self.embed_f = embed_f
-        self.centroid=None
-        self.scale=None
-        self.k = k
-        self.debug=False
-        self.distance = pairwise_euclidean_distances
-    def forward(self, x, A, not_used=None, fixedges=None):
-        x = self.embed_f(x)
-        if self.training:
-            if fixedges is not None:
-                return x, fixedges, torch.zeros(fixedges.shape[0],fixedges.shape[-1]//self.k,self.k,dtype=torch.float,device=x.device)
-            D, _x = self.distance(x)
-            edges_hat, logprobs = self.sample_without_replacement(D)
-        else:
-            with torch.no_grad():
-                if fixedges is not None:
-                    return x, fixedges, torch.zeros(fixedges.shape[0],fixedges.shape[-1]//self.k,self.k,dtype=torch.float,device=x.device)
-                D, _x = self.distance(x)
-                edges_hat, logprobs = self.sample_without_replacement(D)
-        if self.debug:
-            self.D = D
-            self.edges_hat=edges_hat
-            self.logprobs=logprobs
-            self.x=x
-        return x, edges_hat, logprobs
-    def sample_without_replacement(self, logits):
-        b,n,_ = logits.shape
-        logits = logits * torch.exp(torch.clamp(self.temperature,-5,5))
-        q = torch.rand_like(logits) + 1e-8
-        lq = (logits-torch.log(-torch.log(q)))
-        logprobs, indices = torch.topk(-lq,self.k)
-        rows = torch.arange(n).view(1,n,1).to(logits.device).repeat(b,1,self.k)
-        edges = torch.stack((indices.view(b,-1),rows.view(b,-1)),-2)
-        if self.sparse:
-            return (edges+(torch.arange(b).to(logits.device)*n)[:,None,None]).transpose(0,1).reshape(2,-1), logprobs
-        return edges, logprobs
-
-
+        
 class DGM_d(nn.Module):
     def __init__(self, embed_f, k=5, distance="euclidean", sparse=True):
         super(DGM_d, self).__init__()
         
-        self.sparse=sparse
-        
+        self.sparse=sparse        
         self.temperature = nn.Parameter(torch.tensor(1. if distance=="hyperbolic" else 4.).float())
         self.embed_f = embed_f
         self.centroid=None
         self.scale=None
         self.k = k
         self.distance = distance
-        
         self.debug=False
         
     def forward(self, x, A, not_used=None, fixedges=None):
